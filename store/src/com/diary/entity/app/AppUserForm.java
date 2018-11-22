d47038868689061db9a75601c78e4c6b7f87a0cb
zhenjia <zhenjiawang@gmail.com>
2018-11-22 14:14:35 +0800

    123

---
+++ store/src/com/diary/entity/app/AppUserForm.java
@@ -0,0 +1,147 @@
+package com.diary.entity.app;
+
+import com.diary.entity.res.ResFund;
+import org.guiceside.persistence.entity.IdEntity;
+import org.guiceside.persistence.entity.Tracker;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Administrator
+ * Date: 12-8-15
+ * Time: 下午4:26
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "APP_USER_FORM")
+public class AppUserForm extends IdEntity implements Tracker {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private AppUser userId;
+
+    private String formId;
+
+    private Integer year;
+
+    private Integer month;
+
+    private Integer day;
+
+    private Date created;
+
+    private String createdBy;
+
+    private Date updated;
+
+    private String updatedBy;
+
+    private String useYn;
+
+    @Id
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID")
+    public AppUser getUserId() {
+        return userId;
+    }
+
+    public void setUserId(AppUser userId) {
+        this.userId = userId;
+    }
+
+
+    @Column(name = "FORM_ID")
+    public String getFormId() {
+        return formId;
+    }
+
+    public void setFormId(String formId) {
+        this.formId = formId;
+    }
+
+    @Column(name = "YEAR")
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    @Column(name = "MONTH")
+    public Integer getMonth() {
+        return month;
+    }
+
+    public void setMonth(Integer month) {
+        this.month = month;
+    }
+
+    @Column(name = "DAY")
+    public Integer getDay() {
+        return day;
+    }
+
+    public void setDay(Integer day) {
+        this.day = day;
+    }
+
+
+    @Column(name = "CREATED", updatable = false)
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    @Column(name = "CREATED_BY")
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    @Column(name = "UPDATED")
+    public Date getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Date updated) {
+        this.updated = updated;
+    }
+
+    @Column(name = "UPDATED_BY")
+    public String getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    @Column(name = "USE_YN")
+    public String getUseYn() {
+        return useYn;
+    }
+
+    public void setUseYn(String useYn) {
+        this.useYn = useYn;
+    }
+}
---
+++ store/src/com/diary/providers/biz/app/UserFormBiz.java
@@ -0,0 +1,10 @@
+package com.diary.providers.biz.app;
+
+
+import com.diary.common.BizException;
+
+public interface UserFormBiz {
+
+    String submit(Long userId, String formId) throws BizException;
+
+}
